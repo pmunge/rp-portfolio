a539ce811e8506feee2606fb9cef15ffe454b3ee
pmunge <munge.pw620@gmail.com>
2024-09-20 15:46:41 +0300

    added a footer

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ...
 
 MEDIA_ROOT = BASE_DIR / "uploads/"
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
